6421820294b3cd3a440e8b5175bb70dd8a43a73c
Pratyush Kumar Jaiswal <pratyush.jaiswal2525@gmail.com>
2024-06-07 23:59:23 +0530

    Add files via upload

---
+++ ReactApp/src/App.js
@@ -0,0 +1,25 @@
+import EmployeeComponent from "./components/EmployeeComponent";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import { BrowserRouter,Route,Routes } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<ListEmployeeComponent/>}></Route>
+        <Route path="/employees" element={<ListEmployeeComponent/>}></Route>
+        <Route path="/add-employee" element={<EmployeeComponent/>}></Route>
+        <Route path="/edit-employee/:id" element={<EmployeeComponent/>}></Route>
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactApp/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <nav class="navbar navbar-dark bg-dark">
+            <a className='navbar-brand'>Employee Management System</a>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
